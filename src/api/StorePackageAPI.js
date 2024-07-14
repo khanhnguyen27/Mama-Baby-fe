12a7d8541b978d86ef83651ab152bde825a3ec00
Vt-mmm <vinhtamforwork@gmail.com>
2024-07-14 20:53:30 +0700

    Update API buy Packages

--- src/api/StorePackageAPI.js
+++ src/api/StorePackageAPI.js
@@ -2,7 +2,15 @@ import axiosJWT from "./ConfigAxiosInterceptor";
 const URL_STOREPACKAGE = `http://localhost:8080/mamababy/store_package`;
 
 export const allStorePackageApi = (params) => {
-    return axiosJWT.get(URL_STOREPACKAGE, {
-        params: params,
-    });
+  return axiosJWT.get(URL_STOREPACKAGE, {
+    params: params,
+  });
+};
+
+export const createStorePackageApi = (packageId, storeId, price) => {
+  return axiosJWT.post(URL_STOREPACKAGE, {
+    package_id: packageId,
+    store_id: storeId,
+    price: price,
+  });
 };
